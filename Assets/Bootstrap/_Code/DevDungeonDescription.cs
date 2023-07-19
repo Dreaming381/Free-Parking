958c974882b1d78e9c32a6ccfb7ec4cd9fff0f4b
Tyler <dreamingimlatios@gmail.com>
2023-07-19 00:00:33 -0500

    Add dev dungeon description
    Next up, baking and runtime.

---
+++ Assets/Bootstrap/_Code/DevDungeonDescription.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FreeParking
+{
+    public class DevDungeonDescription : ScriptableObject
+    {
+        [Tooltip("The name of the group creating the Dev Dungeon. Additional subgroups for organization can be added with '/' delineation.")]
+        public string groupNameOrPath;
+
+        [Tooltip("The name of the dev dungeon in code.")]
+        public string devDungeonName;
+
+        [Tooltip("The display name of the dev dungeon that the player sees.")]
+        public string devDungeonDisplayName;
+
+        [Tooltip("The creator(s) of the dev dungeon.")]
+        public List<string> creators;
+
+        [Tooltip("An icon for the creator or group of creators.")]
+        public Texture2D creatorIcon;
+
+        [Tooltip("A teaser for the dev dungeon.")]
+        public Texture2D dungeonIcon;
+
+        [Tooltip("Short description of the dev dungeon.")]
+        [TextArea]
+        public string description;
+    }
+}
+
