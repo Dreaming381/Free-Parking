d968a4bd9ced64187093d19c3e48ec5861899d6f
Tyler <dreamingimlatios@gmail.com>
2023-11-25 22:59:48 -0600

    First holiday trip WIP
    The first trip is over, and overall, not a whole lot of progress was made, as most of it was debugging existing systems. There's still one more trip to hopefully get a lot further along.

--- Assets/Dev Dungeons/DreamingImLatios/HolidayExperiments2023/_Code/Holiday2023Components.cs
+++ Assets/Dev Dungeons/DreamingImLatios/HolidayExperiments2023/_Code/Holiday2023Components.cs
@@ -10,5 +10,28 @@ namespace DreamingImLatios.Holiday2023
         public float magnitude;
         public float frequency;
     }
+
+    struct ClothBlob
+    {
+        public BlobArray<int3> triangles;
+        
+        public struct DistanceConstraint
+        {
+            public int indexA;
+            public int indexB;
+            public float targetDistance;
+            public float stiffness;
+        }
+
+        public BlobArray<DistanceConstraint> distanceConstraints;
+    }
+
+    struct Cloth : IComponentData
+    {
+        public BlobAssetReference<ClothBlob> blob;
+        public float friction;
+        public float invMassPerParticle;
+        public float halfThickness;
+    }
 }
 
