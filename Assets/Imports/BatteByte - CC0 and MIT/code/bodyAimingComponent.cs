8c432c299cd81ff2d7ea8c84f4873a0bfc438a8e
Morty <max.miran@gmail.com>
2024-02-18 14:28:12 -0500

    add

---
+++ Assets/Imports/BatteByte - CC0 and MIT/code/bodyAimingComponent.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using Unity.Mathematics;
+using Unity.Entities;
+
+public struct bodyAimingComponent : IComponentData
+{
+    public quaternion rotationExtraAiming; //start with quaternion.identity note only used for inf and tanks but always present
+    public float rotationExtraYAiming;
+}
---
+++ Assets/Imports/BatteByte - CC0 and MIT/code/turretJointComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Collections;
+
+//Mount should be attached to elevator. Rotator should be parent of elevator. This is different than monobehavior because of the way parent system works.
+public struct turretJointComponent : IComponentData
+{
+    public Entity rotator; // parent of elevator
+    public quaternion startingLocalRotationRotator;
+    public Entity elevatorMount; // same entity as mount
+    public quaternion startingLocalRotationElevator;
+}
